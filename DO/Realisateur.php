61b9a3f70ee380aa8d9795cf6e59ddb0f2e0de3f
Youcef <youcefboutiche@hotmail.fr>
2024-11-06 16:38:56 +0100

    ajout DO Resalisateur

--- DO/Realisateur.php
+++ DO/Realisateur.php
@@ -1,8 +1,33 @@
 <?php 
 
 
+class Realisateur {
+    private $id;
+    private $nom;
+    private $prenom;
 
-class Realisateur{
+    public function getId() {
+        return $this->id;
+    }
 
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
 }
 
