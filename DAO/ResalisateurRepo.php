787cc7ee362a455c2b17ef3c0e2a007128fc8ef6
Youcef <youcefboutiche@hotmail.fr>
2024-11-06 16:55:48 +0100

    ajout RealisateurRepo

--- DAO/ResalisateurRepo.php
+++ DAO/ResalisateurRepo.php
@@ -29,9 +29,41 @@ class RealisateurRepo {
             $real->setPrenom($row['prenom']);
             array_push($tab,$real);
         }
-        return $tab;
-
-        
+        return $tab;    
     }
+
+    public function getEmploiById($id){
+        $conn = new Connexion();
+        $sth = $conn->getConnexion()->prepare('SELECT * FROM realisateur WHERE id=?');
+        $sth->execute([$id]);
+
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+        $realisateur = new Realisateur();
+        $realisateur->setId($row['id']);
+        $realisateur->setNom($row['nom']);
+        $realisateur->setPrenom($row['prenom']);
+
+        return $realisateur;
+
+    }
+    public function modifierRealisateur(Realisateur $realisateur){
+        $conn = new Connexion();
+        $stmt= $conn->getConnexion()->prepare("UPDATE realisateur SET nom=:nom,prenom=:prenom WHERE id=:id");
+        $stmt->execute(array(
+            ":id"=> $realisateur->getId(),
+            ":nom"=> $realisateur->getNom(),
+            ":prenom"=>$realisateur->getPrenom()
+        
+        ));
+    }
+
+    public function supprimerRealisateur($id){
+        $conn = new Connexion();
+        $stm = $conn->getConnexion()->prepare('DELETE FROM realisateur WHERE id=?');
+        $stm->execute([$id]);
+
+    }
+
 
 }
